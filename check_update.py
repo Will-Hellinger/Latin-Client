b18c86260a3413ad7787db74bb88f91ee3bc452d
Will-Hellinger <111201130+Will-Hellinger@users.noreply.github.com>
2023-01-07 18:22:01 -0600

    dictionary update and minor update patch

--- check_update.py
+++ check_update.py
@@ -24,44 +24,47 @@ def run():
     doUpdate = True
     try:
         server_chksm = ((requests.get(f'{updateURL}main.chksm').content)).decode("utf-8").replace('\n', '')
+        if 'repository' in server_chksm:
+            doUpdate = False
     except:
         print('[-] Unable to check for updates')
         doUpdate = False
     
-    if doUpdate == True:
-        if os.path.exists(checksumFilename):
-            os.remove(checksumFilename)
-
-        filelist = []
-        for root, dirs, files in os.walk('.'):
-            for file in files:
-                filelist.append(os.path.join(root,file))
-
-        exclude_list = ['.pyc', '.DS_Store', 'settings.json', 'chksm.json', 'main.chksm']
-
-        with open(checksumFilename, 'w') as temp_file:
-            temp_file.write('{\n}')
     
-        with open(checksumFilename, encoding='utf-8', mode='r+') as file:
-            data = json.load(file)
-            for name in filelist:
-                useFile = True
-                for a in range(len(exclude_list)):
-                    if name.endswith(exclude_list[a]) or '.git' in name: #dont want .git files to get tangled into/with mine
-                        useFile = False
-                if useFile == True:
-                    data[name.replace(subDirectory, "(sub)")] = hashlib.md5(open(name,'rb').read()).hexdigest()
-#                    print(f"{name} - {hashlib.md5(open(name,'rb').read()).hexdigest()}")
-            file.seek(0)
-            json.dump(data, file, indent=4)
-            file.truncate()
+    if os.path.exists(checksumFilename):
+        os.remove(checksumFilename)
 
-        chksm = hashlib.md5(str(open(checksumFilename, encoding='utf-8', mode='r').read()).encode('utf-8')).hexdigest()
-        data = json.load(open(checksumFilename, encoding='utf-8', mode='r'))
+    filelist = []
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            filelist.append(os.path.join(root,file))
 
-        with open('main.chksm', encoding='utf-8', mode='w') as file:
-            file.write(str(chksm))
+    exclude_list = ['.pyc', '.DS_Store', 'settings.json', 'chksm.json', 'main.chksm']
 
+    with open(checksumFilename, 'w') as temp_file:
+        temp_file.write('{\n}')
+    
+    with open(checksumFilename, encoding='utf-8', mode='r+') as file:
+        data = json.load(file)
+        for name in filelist:
+            useFile = True
+            for a in range(len(exclude_list)):
+                if name.endswith(exclude_list[a]) or '.git' in name: #dont want .git files to get tangled into/with mine
+                    useFile = False
+            if useFile == True:
+                data[name.replace(subDirectory, "(sub)")] = hashlib.md5(open(name,'rb').read()).hexdigest()
+#               print(f"{name} - {hashlib.md5(open(name,'rb').read()).hexdigest()}")
+        file.seek(0)
+        json.dump(data, file, indent=4)
+        file.truncate()
+
+    chksm = hashlib.md5(str(open(checksumFilename, encoding='utf-8', mode='r').read()).encode('utf-8')).hexdigest()
+    data = json.load(open(checksumFilename, encoding='utf-8', mode='r'))
+
+    with open('main.chksm', encoding='utf-8', mode='w') as file:
+        file.write(str(chksm))
+
+    if doUpdate == True:
         if chksm != server_chksm and build_mode == False:
             print(f'[-] Not up to date...\nCurrent Checksum: {chksm}\nServer Checksum: {server_chksm}\nDynamically updating...')
             newest_chksms = json.loads(requests.get(f'{updateURL}chksm.json').content)
@@ -95,6 +98,8 @@ def run():
                 print('[+] Up to date')
             else:
                 print(f'[+] Building chksms: {server_chksm} -> {chksm}')
+    elif build_mode == True:
+        print(f'[+] Building chksms: Not connected -> {chksm}')
 
 if len(sys.argv) >= 2:
     if str(sys.argv[1]) == 'build':
