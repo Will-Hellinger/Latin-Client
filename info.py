c7e35b5c5354727ab78c67f48483cc772aa840f3
Will Hellinger <will.hellinger@gmail.com>
2023-04-27 21:08:17 -0500

    Info update
    Removes setup file and moves over functions to info, info now has specific functions to use in the python3 terminal too.

--- info.py
+++ info.py
@@ -1,8 +1,10 @@
 import json
-import setup
 import os
 import time
 import random
+import validators
+import shutil
+import glob
 
 version = 3.0
 user = 'none'
@@ -20,10 +22,35 @@ else:
 
 path = f'.{subDirectory}data{subDirectory}'
 
+
 def clear_console():
     os.system(clear)
 
-actionButton = "`"
+#great terminal commands
+def get_file_count(directory: str):
+    directory.replace('(sub)', subDirectory)
+
+    if not directory.endswith(subDirectory):
+        directory += subDirectory
+
+    return len(glob.glob(f'{directory}*.*'))
+
+
+def find_file(file_name: str, ignore_case = False):
+    output = []
+
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            
+            if ignore_case == True:
+                if file_name.lower() in file.lower() and '.git' not in root and '__pycache__' not in root:
+                    output.append(os.path.join(root,file))
+            
+            else:
+                if file_name in file and '.git' not in root and '__pycache__' not in root:
+                    output.append(os.path.join(root,file))
+    
+    return output
 
 
 def load_settings():
@@ -53,20 +80,6 @@ def load_settings():
     latinLink = settings['schoology'].get('latin-link')
     schoologyUser = settings['schoology'].get('username')
     schoologyPass = settings['schoology'].get('password')
-
-
-try:
-    load_settings()
-
-    settings_list = (webbrowserType, delay, human_mode, timed_vocab_fallback, compositions_fallback, discord_rpc, funnySound, latinLink, schoologyUser, schoologyPass)
-    for setting in settings_list:
-        if setting == None or setting == "none":
-            setup.run()
-    
-    load_settings()
-
-except:
-    setup.run()
 
 
 def encodeFilename(file_name: str):
@@ -108,3 +121,93 @@ def human_timeout(min = 1000, max = 5000):
 
     if human_mode == True:
         time.sleep(int(random.randint(min, max))/1000)
+
+
+def check_settings():
+    if not os.path.exists(f'{path}settings.json'):
+        shutil.copyfile(f'.{subDirectory}data{subDirectory}backup{subDirectory}base_settings.json', f'{path}settings.json')
+    
+    try:
+        return json.load(open(f'{path}settings.json', 'r'))
+    except:
+        shutil.copyfile(f'.{subDirectory}data{subDirectory}backup{subDirectory}base_settings.json', f'{path}settings.json')
+    
+    return json.load(open(f'{path}settings.json', 'r'))
+
+
+def get_browser_type():
+    while True:
+        browser_types = ['Chrome', 'Chromium', 'Brave', 'Firefox', 'Internet Explorer', 'Edge', 'Opera', 'Safari']
+
+        print('Browser Types:\n------------')
+        for a, browser_type in enumerate(browser_types):
+            print(f'{a + 1}. {browser_type}')
+        print('------------')
+
+        choice = input('Please enter the number of your browser: ')
+
+        try:
+            index = int(choice) - 1
+            if index >= 0 and index < len(browser_types):
+                return browser_types[index]
+        except:
+            pass
+
+        print('Invalid choice, please try again.')
+
+
+def get_latin_link():
+    while True:
+        link = input('Please enter the Schoology link for the Latin app: ')
+
+        if validators.url(link) == True:
+            return link
+        else:
+            print('Invalid URL, please try again.')
+
+
+def get_schoology_credentials():
+    username = input('Please enter your Schoology username: ')
+    password = input('Please enter your Schoology password: ')
+
+    return {"username" : username, "password" : password}
+
+
+def setup():
+    try:
+        settings = check_settings()
+        with open(f'{path}settings.json', 'r+') as file:
+        
+            if settings['configuration']['browser-type'] == 'none':
+                browser_type = get_browser_type()
+                settings['configuration']['browser-type'] = browser_type
+
+            if settings['schoology']['latin-link'] == 'none':
+                latin_link = get_latin_link()
+                settings['schoology']['latin-link'] = latin_link
+
+            if settings['schoology']['username'] == 'none':
+                creds = get_schoology_credentials()
+                settings['schoology']['username'] = creds["username"]
+                settings['schoology']['password'] = creds["password"]
+            
+            save_file(file, settings)
+
+    except Exception as error:
+        print(f'Error: {error}')
+        exit()
+
+
+
+try:
+    load_settings()
+
+    settings_list = (webbrowserType, delay, human_mode, timed_vocab_fallback, compositions_fallback, discord_rpc, funnySound, latinLink, schoologyUser, schoologyPass)
+    for setting in settings_list:
+        if setting == None or setting == "none":
+            setup()
+    
+    load_settings()
+
+except:
+    setup()
